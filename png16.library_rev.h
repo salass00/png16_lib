8bea0d5440fc0b21843fa9b8384eb797f7f672ca
Fredrik Wikstrom <fredrik@a500.org>
2017-12-25 21:37:12 +0200

    png16.library 53.1 (25.12.2017)

--- png16.library_rev.h
+++ png16.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
 #define REVISION	1
-#define DATE		"24.12.2017"
+#define DATE		"25.12.2017"
 #define VERS		"png16.library 53.1"
-#define VSTRING		"png16.library 53.1 (24.12.2017)\r\n"
-#define VERSTAG		"\0$VER: png16.library 53.1 (24.12.2017)"
+#define VSTRING		"png16.library 53.1 (25.12.2017)\r\n"
+#define VERSTAG		"\0$VER: png16.library 53.1 (25.12.2017)"
