c2fcf6fdff35bcf2aa7e61552b5ce8ccb732535c
Fredrik Wikstrom <fredrik@a500.org>
2023-01-01 11:54:38 +0200

    Bumped version to 53.3.

--- png16.library_rev.h
+++ png16.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	2
-#define DATE		"4.12.2022"
-#define VERS		"png16.library 53.2"
-#define VSTRING		"png16.library 53.2 (4.12.2022)\r\n"
-#define VERSTAG		"\0$VER: png16.library 53.2 (4.12.2022)"
+#define REVISION	3
+#define DATE		"1.1.2023"
+#define VERS		"png16.library 53.3"
+#define VSTRING		"png16.library 53.3 (1.1.2023)\r\n"
+#define VERSTAG		"\0$VER: png16.library 53.3 (1.1.2023)"
